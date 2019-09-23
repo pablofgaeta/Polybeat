9aae22efec55914059fe222dc4a25b496e57a7bf
pablofgaeta <pablofgaeta@gmail.com>
2019-09-22 19:21:30 -0700

    Each box can store its own divison for polyrhythm

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 let polyapp, myCanvas;
 let ids = [];
+let boxes = [];
+let divisions = [];
+let divDisplays = [];
+let startDiv = 4;
+
 
 function setup() {
 	myCanvas = createCanvas(0, 0);
@@ -9,11 +14,10 @@ function setup() {
 }
 
 function draw() {
-	// push();
-	// noStroke();
-	// fill(255,0,255);
-	// ellipse(25,25,25);
-	// pop();
+	// Update each divisions display
+	for (var i = 0; i < divDisplays.length; i++) {
+		divDisplays[i].html('Divisions: ' + divisions[i].value());
+	}
 }
 
 class Poly {
@@ -22,40 +26,51 @@ class Poly {
 
 		this.add = createImg('./pngs/add.png').position(10,10).size(130,100).style('border-radius', '10px');
 		this.add.mousePressed(this.createPoly);
-		this.markListener(this.add);
-
+		markListener(this.add);
 		
 		this.header = select("#header").html(this.title);
 	}
 	createPoly(){
 
-		var polybox = createElement('div', '').addClass('polybox');
+		let polybox = createElement('div', '').addClass('polybox').value((boxes.length + 1));
+		boxes.push(polybox);
 
-		var exit = createImg('./pngs/exit.png').addClass('exit').parent(polybox);
+		let exit = createImg('./pngs/exit.png').addClass('exit').parent(polybox);
 
-		var id = createElement('div', 'beat: ' + (ids.length + 1)).addClass('id').parent(polybox).value((ids.length + 1));
-
+		let id = createElement('div', 'beat: ' + (boxes.length)).addClass('boxtxt').parent(polybox).position(0, 10).style('font-size', '30px').center('horizontal');
 		ids.push(id);
 
+		let division = createSlider(1, 15, startDiv, 1).addClass('division').parent(polybox).position(0,60).center('horizontal'); 
+		divisions.push(division);
+
+		let divDisplay = createElement('div', 'Division: ' + startDiv).addClass('boxtxt').addClass('divDisplay').parent(polybox).position(0, 85).style('font-size', '15px').center('horizontal');
+		divDisplays.push(divDisplay);
+
 		exit.mousePressed(() => {
+			// Delete
+			ids.splice(polybox.value() - 1, 1);
+			boxes.splice(polybox.value() - 1,1);
+			divisions.splice(polybox.value() - 1, 1);
+			divDisplays.splice(polybox.value() - 1, 1);
 			polybox.remove();
-			exit.remove();
-			ids.splice(id.value()-1,1);
-			id.remove();
-			for(var i = 0; i < ids.length; i++) {
+
+			// Reassign
+			for(var i = 0; i < boxes.length; i++) {
 				ids[i].html("beat: " + (i+1));
+				boxes[i].value(i+1);
 				ids[i].value(i+1);
 			}
 		});
 	}
-	markListener(element){
-		element.mouseOver(() => {
-			element.style('background-color', "#F2C9ED");
-		})
-		element.mouseOut(() => {
-			element.style('background-color', "#FFFFFF");
-		})
-	}
+	
+}
+function markListener(element){
+	element.mouseOver(() => {
+		element.style('background-color', "#F2C9ED");
+	})
+	element.mouseOut(() => {
+		element.style('background-color', color(0,0,0,0));
+	})
 }
 
 
